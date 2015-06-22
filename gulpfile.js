43c954bd7121a4d16ab9c4016996a3d5bdb0e490
stianlagstad <stianlagstad@gmail.com>
2015-06-22 15:51:17 +0200

    ch ch ch ch changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,43 @@
-var gulp = require('gulp'),
-	notify = require('gulp-notify'),
-	bower = require('gulp-bower');
+// Include gulp
+var gulp = require('gulp');
 
-var config = {
-	bowerDir: './bower_components'
-}
+// Include plugins
+var notify = require('gulp-notify'),
+	bower = require('gulp-bower'),
+	jshint = require('gulp-jshint'),
+	sass = require('gulp-sass'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename');
 
-gulp.task('bower', function() {
-	return bower()
-		.pipe(gulp.dest(config.bowerDir))
+// Lint task
+gulp.task('lint', function() {
+	return gulp.src('js/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
 });
 
-gulp.task('default', ['bower']);
+// Compile sass
+gulp.task('sass', function() {
+	return gulp.src('scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('css'));
+});
+
+// Concatenate & minify JS
+gulp.task('scripts', function() {
+	return gulp.src('js/*.js')
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('dist'))
+		.pipe(rename('all.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
+});
+
+// Watch files for changes
+gulp.task('watch', function() {
+	gulp.watch('js/*.js', ['lint', 'scripts']);
+	gulp.watch('scss/*.scss', ['sass']);
+});
+
+// Default task
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);

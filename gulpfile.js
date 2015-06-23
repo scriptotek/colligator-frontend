44101365b18d3138ea4fdd481cbfbc303870bd85
stianlagstad <stianlagstad@gmail.com>
2015-06-23 08:49:38 +0200

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,7 @@ var gulp = require('gulp');
 // Include plugins
 var notify = require('gulp-notify'),
 	bower = require('gulp-bower'),
-	jshint = require('gulp-jshint'),
-	sass = require('gulp-sass'),
-	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename');
+	jshint = require('gulp-jshint');
 
 // Lint task
 gulp.task('lint', function() {
@@ -16,28 +13,16 @@ gulp.task('lint', function() {
 		.pipe(jshint.reporter('default'));
 });
 
-// Compile sass
-gulp.task('sass', function() {
-	return gulp.src('scss/*.scss')
-		.pipe(sass())
-		.pipe(gulp.dest('css'));
-});
-
-// Concatenate & minify JS
-gulp.task('scripts', function() {
-	return gulp.src('js/*.js')
-		.pipe(concat('all.js'))
-		.pipe(gulp.dest('dist'))
-		.pipe(rename('all.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist'));
+// Gulp task
+gulp.task('bower', function() {
+	return bower()
+		.pipe(gulp.dest('?????????????????????????????????????????'))
 });
 
 // Watch files for changes
 gulp.task('watch', function() {
 	gulp.watch('js/*.js', ['lint', 'scripts']);
-	gulp.watch('scss/*.scss', ['sass']);
 });
 
 // Default task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+gulp.task('default', ['lint', 'watch']);

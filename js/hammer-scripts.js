b8d48710e42213c72283973b7ac595958a5eb471
stianlagstad <stianlagstad@gmail.com>
2015-06-24 11:54:48 +0200

    Carousel pollutes global scope for now

--- js/hammer-scripts.js
+++ js/hammer-scripts.js
@@ -5,9 +5,9 @@ var hammerOverlay = document.getElementById('hammerOverlay');
 var mc = new Hammer(hammerOverlay);
 mc.on('swipeleft', function(ev) {
 	console.log('swipeleft');
-	carouselObject.rotate(-1);
+	rotateCarousel(-1);
 });
 mc.on('swiperight', function(ev) {
 	console.log('swiperight');
-	carouselObject.rotate(1);
+	rotateCarousel(1);
 });
